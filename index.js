d52b283a92e5a556fdfb8c0329b2c427abe03246
Luke Baker <lukebaker@gmail.com>
2020-07-24 17:22:12 -0400

    Add creating of sample issue

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+const github = require("@actions/github");
 const core = require("@actions/core");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    core.info(new Date().toTimeString());
+    const token = core.getInput("token");
+    const repository = core.getInput("repository");
+    const user = core.getInput("user");
+
+    const octokit = github.getOctokit(token);
+    const [owner, repo] = repository.split("/");
+    const newIssue = await octokit.issues.create({
+      owner,
+      repo,
+      assignee: user,
+      title: `New issue for ${user}!`,
+      body: `Here's the body. @${user}`,
+    });
+    await octokit.issues.update({
+      owner,
+      repo,
+      issue_number: newIssue.data.id,
+      state: "closed",
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.test.js
+++ index.test.js
@@ -1,9 +1,1 @@
-const process = require("process");
-const cp = require("child_process");
-const path = require("path");
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test("test runs", () => {
-  const ip = path.join(__dirname, "index.js");
-  console.log(cp.execSync(`node ${ip}`, { env: process.env }).toString());
-});
+test("sample test", () => {});
