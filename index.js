c0f947447b80f19394b12e5c738d8a808cd4eb5c
Luke Baker <lukebaker@gmail.com>
2020-07-24 22:16:48 -0400

    Add searching for existing issue

--- index.js
+++ index.js
@@ -17,38 +17,61 @@ const findIssues = `query findIssues($owner: String!, $repo: String!, $mentioned
 }
 `;
 
+async function findExistingIssue(octokit, { owner, repo, user, title }) {
+  let issue = null;
+  let hasNextPage = true;
+  let after = null;
+  while (!issue && hasNextPage) {
+    let findIssuesResp = await octokit.graphql(findIssues, {
+      owner,
+      repo,
+      after,
+      mentioned: user,
+    });
+    hasNextPage = findIssuesResp.repository.issues.pageInfo.hasNextPage;
+    after = findIssuesResp.repository.issues.pageInfo.endCursor;
+    issue = findIssuesResp.repository.issues.nodes.find(
+      (i) => i.title === title
+    );
+  }
+  return issue;
+}
+
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     const token = core.getInput("token");
     const repository = core.getInput("repository");
     const user = core.getInput("user");
+    const title = core.getInput("title");
 
     const octokit = github.getOctokit(token);
     const [owner, repo] = repository.split("/");
-    let issues = await octokit.graphql(findIssues, {
-      owner,
-      repo,
-      mentioned: user,
-    });
-    core.info(JSON.stringify(issues));
 
-    /*
-    const newIssue = await octokit.issues.create({
+    const existingIssue = await findExistingIssue(octokit, {
       owner,
       repo,
-      assignee: user,
-      title: `New issue for ${user}!`,
-      body: `Here's the body. @${user}`,
+      user,
+      title,
     });
+    if (existingIssue) {
+      core.info(JSON.stringify(existingIssue));
+    } else {
+      const newIssue = await octokit.issues.create({
+        owner,
+        repo,
+        assignee: user,
+        title,
+        body: `Here's the body. @${user}`,
+      });
 
-    await octokit.issues.update({
-      owner,
-      repo,
-      issue_number: newIssue.data.number,
-      state: "closed",
-    });
-    */
+      await octokit.issues.update({
+        owner,
+        repo,
+        issue_number: newIssue.data.number,
+        state: "closed",
+      });
+    }
   } catch (error) {
     core.setFailed(error);
   }
