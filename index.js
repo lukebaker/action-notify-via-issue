78f79160fe86ca999078c6699cd8b589ef676f2d
Luke Baker <lukebaker@gmail.com>
2020-07-26 16:55:09 -0400

    Switch to debug log

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function run() {
       strategy,
       matrix,
     };
-    core.info(JSON.stringify(templateContext));
+    core.debug(JSON.stringify(templateContext));
     const title = template(core.getInput("title"))(templateContext);
     templateContext = { ...templateContext, title };
 
